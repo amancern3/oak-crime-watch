0154d7cd5c6a922d07b82d98145bfe1d7fd4b1d1
amancern3 <amandeep842.as@gmail.com>
2022-10-06 23:13:29 -0700

    py script -> writes to pd db to csv

---
+++ public/scripts/oak-crime.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+from dotenv import load_dotenv
+import pandas as pd
+from sodapy import Socrata
+
+
+#init env sys vars
+def init_env(file, vars):
+    load_dotenv(file)
+    if vars is not None:
+        for env in vars:
+            if "=" in env:
+                env = env.split("=", 1)
+                os.environ[env[0]] = env[1]
+
+
+path = "/Users/bitterfq/oak-crime-watch/.env"
+init_env(path, ['API_KEY_ID', 'API_KEY_SECRET',
+          'APP_TOKEN', 'APP_SECRET_TOKEN', 'USER_NAME', 'PASSWRD'])
+
+# authenticated client :
+client = Socrata("data.oaklandca.gov"   ,
+                  os.environ['APP_TOKEN'],
+                  os.environ['USER_NAME'],
+                  os.environ['PASSWRD'])
+
+# First 2000 results, returned as JSON from API / converted to Python list of
+# dictionaries by sodapy.
+results = client.get("ym6k-rx7a", limit=2000)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
+results_df.to_csv('public/scripts/out.csv')
+
+print("EXIT(0)")
